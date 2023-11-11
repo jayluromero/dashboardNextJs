b1368f670af1ca2583b276bf7b26c6158b6eebc2
jayluromero <jayluromero@icloud.com>
2023-11-11 13:14:32 -0600

    cambios LearnNEXTJS

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Cairo, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const cairo = Cairo({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
